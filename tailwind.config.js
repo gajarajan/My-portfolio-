1139299fb5a47ae1a1f33d61b60ede93e9f2020f
gajarajan <gajarajan2000@gmail.com>
2024-11-26 16:58:11 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 10s ease infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%': {
+            backgroundPosition: '0% 50%',
+          },
+          '50%': {
+            backgroundPosition: '100% 50%',
+          },
+          '100%': {
+            backgroundPosition: '0% 50%',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
